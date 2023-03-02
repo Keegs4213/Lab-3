f0048d7aecf725693f88961387cb985538c7ec6f
Keegan <keegs4213@gmail.com>
2023-03-02 19:04:36 +1300

    completed up to exercise 6

--- intermediate.js
+++ intermediate.js
@@ -168,4 +168,50 @@
 // }  
 // console.log(currencyOperation(0.5, 0.6, "*"))
 
-//d
+// //d
+// function currencyOperation(float1, float2, operation, numDecimals) {
+//     let factor = Math.pow(10,numDecimals);
+//     let result;
+//     switch(operation) {
+//         case "+":
+//             result = (float1 * factor + float2 * factor) / factor;
+//             break;
+//         case "-":
+//             result = (float1 * factor - float2 * factor) / factor;
+//             break;
+//         case "*":
+//             result = (float1 * float2).toFixed(numDecimals);
+//             break;
+//         case "/":
+//             result = (float1 / float2).toFixed(numDecimals);
+//             break;    
+//         default:
+//             console.log("invalid operation")  
+//     }
+//     return result;
+// }  
+// console.log(currencyOperation(0.5, 0.6, "*", 2))
+
+// 6. Create a function unique(duplicatesArray) which takes an array parameter that may
+// include duplicates. Your function should return an array containing only the unique values
+// from duplicatesArray.
+// Test with the following arrays and create another one of your own.
+// const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow']
+// const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43]
+
+// function unique(duplicatesArray) {
+//     const uniqueArray = [];
+  
+//     for (let i = 0; i < duplicatesArray.length; i++) {
+//       if (!uniqueArray.includes(duplicatesArray[i])) {
+//         uniqueArray.push(duplicatesArray[i]);
+//       }
+//     }
+  
+//     return uniqueArray;
+//   }
+
+
+
+// console.log(unique(colours)) // [ 'red', 'green', 'blue', 'yellow', 'orange' ]
+// console.log(unique(testScores)) // [ 55, 84, 97, 63, 32, 91, 43 ]
