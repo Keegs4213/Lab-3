6b7db53aebc0a9b3d315ef3ff186f942befe28df
Keegan <keegs4213@gmail.com>
2023-03-10 20:26:14 +1300

    completed question 7 advanced

--- advanced.js
+++ advanced.js
@@ -359,3 +359,65 @@
 //     console.log(a * b * c * d)
 // }
 // multiply.delay(500)(2, 3, 4, 5)  // prints 120 after 500 milliseconds
+
+
+// 7. In JavaScript, the toString method is used to convert an object to a string representation.  By default, when an object is converted to a String, it returns a string that looks something  like [object Object].  
+// However, we can define our own toString methods for custom objects to provide a more  meaningful string representation. 
+// a) Define a custom toString method for the Person object that will format and print  their details 
+// b) Test your method by creating 2 different people using the below constructor function  and printing them 
+// c) Create a new constructor function Student that uses call to inherit from Person and  add an extra property cohort
+// d) Add a custom toString for Student objects that formats and prints their details. Test  with 2 students. 
+// function Person(name, age, gender) { 
+// this.name = name; 
+// this.age = age; 
+// this.gender = gender; 
+// } 
+// const person1 = new Person('James Brown', 73, 'male') 
+// console.log('person1: '+person1) //prints person1: [object Object] 
+
+
+//a
+// function Person(name, age, gender) { 
+//     this.name = name; 
+//     this.age = age; 
+//     this.gender = gender; 
+    
+//     this,toString = function() {
+//         return "${this.name} (${this.age}, ${this.gender})"
+//     }
+// }
+
+//b
+// const person1 = new Person("Keegan Greig", 22, "Male");
+// console.log(person1.toString());
+
+// const person2 = new Person("Charlotte Christie", 22, "Female");
+// console.log(person2.toString());
+
+// function Person(name, age, gender) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+
+    
+//     this.toString = function() {
+//       return `${this.name} (${this.age}, ${this.gender}), ${this.cohort})`;
+//     }
+//   }
+
+// //c
+
+// function Student(name, age, gender, cohort) {
+//     Person.call(this, name, age, gender);
+//     this.cohort = cohort;
+//   }
+
+
+// //D
+
+
+// const student1 = new Student('Keegan Greig', 22, 'Male', 'SE2023');
+// console.log(student1.toString()); 
+
+// const student2 = new Student('Charlotte', 22, 'Female', 'SE2023');
+// console.log(student2.toString()); 
